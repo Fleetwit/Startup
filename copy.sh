3a933f3a73afe0143fa86186064813d209e04799
Twenty-Six medias, Inc. <julien@twenty-six-medias.com>
2013-07-07 18:56:40 -0300

    Release

---
+++ copy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cp Datastore.conf /etc/init/Datastore.conf
+cp Agora.conf /etc/init/Agora.conf
+cp Daemons.conf /etc/init/Daemons.conf
+cp Mailstack.conf /etc/init/Mailstack.conf
+cp Stats.conf /etc/init/Stats.conf
